2b12aff94540e8ada8526d738f50a9b8ef63bbf8
neeld <neeld@Ordi-de-Damien.home>
2023-02-04 15:44:53 +0100

    Phase 3 : Ajouter une fonctionnalité Task #6 -  gérer les catégories

---
+++ src/Controller/admin/AdminMediatekcategorieController.php
@@ -0,0 +1,97 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+namespace App\Controller\admin;
+
+use App\Entity\Categorie;
+use App\Entity\Formation;
+use App\Entity\Playlist;
+use App\Form\CategorieType;
+use App\Form\PlaylistType;
+use App\Repository\CategorieRepository;
+use App\Repository\FormationRepository;
+use App\Repository\PlaylistRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Description of AdminMediatekcategorieController
+ *
+ * @author neeld
+ */
+class AdminMediatekcategorieController extends AbstractController  {
+     /**
+     * 
+     * @var PlaylistRepository
+     */
+    private $playlistRepository;
+    
+    /**
+     * 
+     * @var FormationRepository
+     */
+    private $formationRepository;
+    
+    /**
+     * 
+     * @var CategorieRepository
+     */
+    private $categorieRepository;
+    
+    function __construct(PlaylistRepository $playlistRepository, CategorieRepository $categorieRepository, FormationRepository $formationRepository) {
+        $this->playlistRepository = $playlistRepository;
+        $this->categorieRepository= $categorieRepository;
+        $this->formationRepository= $formationRepository;
+    }
+    
+    /**
+     * @Route("/admin/categorie", name="admin.categorie")
+     * @return Response
+     */
+    public function index(): Response{
+        $playlists = $this->playlistRepository->findAll();
+        $categories = $this->categorieRepository->findAll();
+        return $this->render("admin/admin.categorie.html.twig", [
+            'playlists' => $playlists,
+            'categories' => $categories
+        ]);
+    }
+    
+     /**
+     * @Route("/admin/suppr/categorie/{id}", name="admin.categorie.suppr")
+     * @param Categorie $categorie
+     * @return Response
+     */
+    public function suppr(Categorie $categorie): Response{
+        $this->categorieRepository->remove($categorie, true);
+        return $this->redirectToRoute('admin.categorie');
+    }
+    
+    /**
+     * @Route("/admin/ajout/categorie}", name="admin.categorie.ajout")     
+     * @param Request $request
+     * @return Response
+     */
+    public function ajout(Request $request): Response{
+        $categorie = new Categorie();
+        $formCategorie = $this->createForm(CategorieType::class, $categorie);
+        
+        $formCategorie->handleRequest($request);       
+            
+        if($formCategorie->isSubmitted() && $formCategorie->isValid()){
+            $this->categorieRepository->add($categorie, true);
+            return $this->redirectToRoute('admin.categorie');
+        }
+           
+        return $this->render("admin/admin.categorie.ajout.html.twig", [
+            'categorie' => $categorie,
+            'formCategorie' => $formCategorie->createView()
+        ]); 
+    }
+}
--- src/Repository/CategorieRepository.php
+++ src/Repository/CategorieRepository.php
@@ -23,6 +23,12 @@ class CategorieRepository extends ServiceEntityRepository
 
     public function add(Categorie $entity, bool $flush = false): void
     {
+        foreach ($this->findAll() as $categorie) {
+            $name=$categorie->getName();
+            if($entity->getName()== $name){
+                return;
+            }
+        }
         $this->getEntityManager()->persist($entity);
 
         if ($flush) {
@@ -32,7 +38,12 @@ class CategorieRepository extends ServiceEntityRepository
 
     public function remove(Categorie $entity, bool $flush = false): void
     {
-        $this->getEntityManager()->remove($entity);
+            if($entity->getFormations()->isEmpty()){
+                $this->getEntityManager()->remove($entity);
+            }else{
+                
+            }
+         
 
         if ($flush) {
             $this->getEntityManager()->flush();
@@ -55,4 +66,14 @@ class CategorieRepository extends ServiceEntityRepository
                 ->getResult();        
     }    
 
+    /**
+     * Retourne la liste des catégories 
+     * @return array
+     */
+    public function findAll(): array{
+        return $this->createQueryBuilder('c') 
+                ->orderBy('c.name', 'ASC')   
+                ->getQuery()
+                ->getResult();        
+    }    
 }
